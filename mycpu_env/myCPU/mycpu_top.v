9d707dc8900c704e21ed70305e3e8337f4e1acc9
lsl <lsl@lsl.com>
2025-07-09 17:55:35 +0800

    feat: added slti,sltui,andi,ori,xori,sll.w,srl.w,sra.w,pcaddu12i

--- mycpu_env/myCPU/mycpu_top.v
+++ mycpu_env/myCPU/mycpu_top.v
@@ -102,6 +102,19 @@ module mycpu_top #
     wire        inst_bne;
     wire        inst_lu12i_w;
 
+	wire		inst_slti;
+	wire		inst_sltui;
+
+	wire		inst_andi;
+	wire		inst_ori;
+	wire		inst_xori;
+
+	wire		inst_sll_w;
+	wire		inst_srl_w;
+	wire		inst_sra_w;
+
+	wire		inst_pcaddu12i;
+
     wire        need_ui5;
     wire        need_si12;
     wire        need_si16;
@@ -188,30 +201,45 @@ module mycpu_top #
     assign inst_bne    = op_31_26_d[6'h17];
     assign inst_lu12i_w= op_31_26_d[6'h05] & ~inst[25];
 
+	assign inst_slti   = op_31_26_d[6'h00] & op_25_22_d[4'h8];
+	assign inst_sltui  = op_31_26_d[6'h00] & op_25_22_d[4'h9];
+
+	assign inst_andi   = op_31_26_d[6'h00] & op_25_22_d[4'hd];
+	assign inst_ori    = op_31_26_d[6'h00] & op_25_22_d[4'he];
+	assign inst_xori   = op_31_26_d[6'h00] & op_25_22_d[4'hf];
+
+	assign inst_sll_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h0e];
+	assign inst_srl_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h0f];
+	assign inst_sra_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h10];
+
+	assign inst_pcaddu12i = op_31_26_d[6'h07] & ~inst[25];
+
     assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
-           | inst_jirl | inst_bl;
+           | inst_jirl | inst_bl | inst_pcaddu12i;
     assign alu_op[ 1] = inst_sub_w;
-    assign alu_op[ 2] = inst_slt;
-    assign alu_op[ 3] = inst_sltu;
-    assign alu_op[ 4] = inst_and;
+    assign alu_op[ 2] = inst_slt | inst_slti;
+    assign alu_op[ 3] = inst_sltu | inst_sltui;
+    assign alu_op[ 4] = inst_and | inst_andi;
     assign alu_op[ 5] = inst_nor;
-    assign alu_op[ 6] = inst_or;
-    assign alu_op[ 7] = inst_xor;
-    assign alu_op[ 8] = inst_slli_w;
-    assign alu_op[ 9] = inst_srli_w;
-    assign alu_op[10] = inst_srai_w;
+    assign alu_op[ 6] = inst_or | inst_ori;
+    assign alu_op[ 7] = inst_xor | inst_xori;
+    assign alu_op[ 8] = inst_slli_w | inst_sll_w;
+    assign alu_op[ 9] = inst_srli_w | inst_srl_w;
+    assign alu_op[10] = inst_srai_w | inst_sra_w;
     assign alu_op[11] = inst_lu12i_w;
 
     assign need_ui5   =  inst_slli_w | inst_srli_w | inst_srai_w;
-    assign need_si12  =  inst_addi_w | inst_ld_w | inst_st_w;
+	assign need_ui12  =  inst_andi | inst_ori | inst_xori;
+    assign need_si12  =  inst_addi_w | inst_ld_w | inst_st_w | inst_slti | inst_sltui;
     assign need_si16  =  inst_jirl | inst_beq | inst_bne;
-    assign need_si20  =  inst_lu12i_w;
+    assign need_si20  =  inst_lu12i_w | inst_pcaddu12i;
     assign need_si26  =  inst_b | inst_bl;
     assign src2_is_4  =  inst_jirl | inst_bl;
 
     assign imm = src2_is_4 ? 32'h4                      :
            need_si20 ? {i20[19:0], 12'b0}         :
            need_si12 ? {{20{i12[11]}}, i12[11:0]}  :
+		   need_ui12 ? {20'b0, i12[11:0]} :
            /*need_ui5*/ {27'b0,i12[4:0]};
 
     assign br_offs = need_si26 ? {{ 4{i26[25]}}, i26[25:0], 2'b0} :
@@ -221,7 +249,7 @@ module mycpu_top #
 
     assign src_reg_is_rd = inst_beq | inst_bne | inst_st_w;
 
-    assign src1_is_pc    = inst_jirl | inst_bl;
+    assign src1_is_pc    = inst_jirl | inst_bl | inst_pcaddu12i;
 
     assign src2_is_imm   = inst_slli_w |
            inst_srli_w |
@@ -231,10 +259,21 @@ module mycpu_top #
            inst_st_w   |
            inst_lu12i_w|
            inst_jirl   |
-           inst_bl     ;
+           inst_bl     |
+		   inst_slti   |
+		   inst_sltui  |
+		   inst_andi   |
+		   inst_ori    |
+		   inst_xori   |
+		   inst_pcaddu12i;
 
     //（其实 ex_forward表示前递alu结果，mem_forward表示前递内存读结果）
-    assign ex_forward = inst_add_w | inst_sub_w | inst_slt | inst_sltu | inst_nor | inst_and | inst_or | inst_xor | inst_slli_w | inst_srli_w | inst_srai_w | inst_addi_w | inst_jirl | inst_bl | inst_lu12i_w;
+    assign ex_forward = inst_add_w | inst_sub_w | inst_slt |
+	 inst_sltu | inst_nor | inst_and | inst_or |
+	   inst_xor | inst_slli_w | inst_srli_w | inst_srai_w |
+		inst_addi_w | inst_jirl | inst_bl | inst_lu12i_w |
+		inst_slti | inst_sltui | inst_andi | inst_ori |
+		inst_xori | inst_sll_w | inst_srl_w | inst_sra_w | inst_pcaddu12i;
     assign mem_forward = inst_ld_w;
     assign wb_forward = 0;
 
@@ -368,8 +407,8 @@ module mycpu_top #
     // 3.rj | rd = dest�?
     // st,beq,bne
 
-    wire rjk_dest_inst = inst_add_w | inst_sub_w | inst_slt | inst_sltu | inst_nor | inst_and | inst_or | inst_xor;
-    wire rj_dest_inst = inst_slli_w | inst_srli_w | inst_srai_w | inst_addi_w | inst_ld_w | inst_jirl;
+    wire rjk_dest_inst = inst_add_w | inst_sub_w | inst_slt | inst_sltu | inst_nor | inst_and | inst_or | inst_xor | inst_sll_w | inst_srl_w | inst_sra_w;
+    wire rj_dest_inst = inst_slli_w | inst_srli_w | inst_srai_w | inst_addi_w | inst_ld_w | inst_jirl | inst_slti | inst_sltui | inst_andi | inst_ori | inst_xori;
     wire rjd_dest_inst = inst_st_w | inst_beq | inst_bne;
     wire is_data_related =
          (rjk_dest_inst & (( ex_valid == 1 && (rj == ex_dest || rk == ex_dest) ) ||
